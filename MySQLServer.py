e6ecdb1b1697bd33511ae8e80ffe11941c524947
Tochukwu <nwibetochukwu@gmail.com>
2024-10-24 17:34:11 +0100

    Let's Build Your Database: Your Gateway to Data Adventure!

--- MySQLServer.py
+++ MySQLServer.py
@@ -19,7 +19,7 @@ def create_database():
             # Print success message
             print("Database 'alx_book_store' created successfully!")
     
-    except Error as e:
+    except mysql.connector.Error as e:  # Specific exception handling for MySQL
         print(f"Error while connecting to MySQL: {e}")
     
     finally:
