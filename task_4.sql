c3b83db7a5137bdd92a16f59b844da84d40ddbfe
Tochukwu <nwibetochukwu@gmail.com>
2024-10-24 17:57:04 +0100

    Create Your Magical Tables

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,5 @@
--- Retrieve full description of the books table
+-- Retrieve full description of the Books table in the alx_book_store database
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'books'
-AND TABLE_SCHEMA = DATABASE();
+WHERE TABLE_NAME = 'Books'
+AND TABLE_SCHEMA = 'alx_book_store';
