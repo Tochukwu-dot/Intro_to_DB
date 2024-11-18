075780e814c04336cee0cde4c36f16e22e6d273c
Tochukwu <nwibetochukwu@gmail.com>
2024-11-18 15:14:20 +0000

    Populating Your Database with Your Very First Data

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,6 @@
+USE alx_book_store;
+
 -- Insert a single row into the customers table
+
 INSERT INTO customers (customer_id, first_name, last_name, email, address)
 VALUES (1, 'Cole', 'Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
